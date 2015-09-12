5edcc87262462328b98fae1bfc6e45938ad671cf
Vaibhav <vaibhavbansal1993@gmail.com>
2015-09-12 22:15:53 +0530

    authorize get call

--- client/admin/assets/js/services/authorize.js
+++ client/admin/assets/js/services/authorize.js
@@ -1,14 +1,15 @@
 'use strict';
-app.service('ngAuthorize',["$localStorage","$http",function($localStorage,$http){
+app.service('ngAuthorize',["$localStorage","$http","$location",function($localStorage,$http,$location){
 	var string = 'hello jiii';
 	this.printString = function(){
 		console.log(string);
 	}
 	this.getRole = function(deferred){
 		var access_token = $localStorage.accessToken;
-		return $http.get('http://localhost:3000/api/getRole?access_token='+access_token).then(function(res){
+
+		return $http.get($location.protocol()+'://'+$location.host()+':'+$location.port()+'/api/getRole?access_token='+access_token)
+		.then(function(res){
 			if(res.data==='admin'){
-				console.log('Authorized');
 				deferred.resolve();
 			}
 			else{
