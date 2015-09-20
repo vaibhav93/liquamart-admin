4f2e54a3c67ef18593805a2700634d5ef853b3ae
Vaibhav <vaibhavbansal1993@gmail.com>
2015-09-21 01:25:08 +0530

    add date to purchase api

--- common/models/purchase.js
+++ common/models/purchase.js
@@ -1,15 +1,21 @@
 var app = require('../../server/server');
+var moment = require('moment');
 module.exports = function(Purchase) {
 	Purchase.beforeRemote('create',function(ctx,unused,next){
-		//console.log(ctx.args);
+		var now = moment(new Date());
+		var date = now.format("D MMM YYYY");
+		console.log(date);
 		var UserModel = app.models.User;
 		UserModel.relations.accessTokens.modelTo.findById(ctx.req.accessToken.id,function(err,accessToken){
-			if(err || ctx.args.data.userId!=accessToken.userId){
-				next(new Error('userId and token mismatch.'));
-			}
-			else{
-				next();
-			}
+			ctx.args.data.userId = accessToken.userId;
+			ctx.args.data.date = date;
+			next();
+			// if(err || ctx.args.data.userId!=accessToken.userId){
+			// 	next(new Error('userId and token mismatch.'));
+			// }
+			// else{
+			// 	next();
+			// }
 		});
 		
 	})
