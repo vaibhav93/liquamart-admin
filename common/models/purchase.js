1121e6d3bf209f8a00199c83cf176b6f901fff4f
Vaibhav <vaibhavbansal1993@gmail.com>
2015-09-21 00:26:55 +0530

    purchase api protection

--- common/models/purchase.js
+++ common/models/purchase.js
@@ -1,3 +1,16 @@
+var app = require('../../server/server');
 module.exports = function(Purchase) {
-
-};
+	Purchase.beforeRemote('create',function(ctx,unused,next){
+		//console.log(ctx.args);
+		var UserModel = app.models.User;
+		UserModel.relations.accessTokens.modelTo.findById(ctx.req.accessToken.id,function(err,accessToken){
+			if(err || ctx.args.data.userId!=accessToken.userId){
+				next(new Error('userId and token mismatch.'));
+			}
+			else{
+				next();
+			}
+		});
+		
+	})
+};;
--- server/middleware/customMW.js
+++ server/middleware/customMW.js
@@ -1,6 +1,6 @@
 module.exports = function() {
   return function myMiddleware(req, res, next) {
-   // console.log('custom MW running');
+    console.log('custom MW running');
    // console.log(res);
     next();
   }
