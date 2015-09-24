9c6efd582f45c55064409a274bf1ff4def06913a
Vaibhav <vaibhavbansal1993@gmail.com>
2015-09-24 15:20:35 +0530

    old mogo

--- server/boot/sample-products.js
+++ server/boot/sample-products.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   // define a custom scope
   
 
-  app.dataSources.mongo.autoupdate('product', function(err) {
+  app.dataSources.mongo.automigrate('product', function(err) {
     if (err) throw err;
 
 
